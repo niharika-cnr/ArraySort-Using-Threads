89b39256ad7156cf5a8db9336b7bd2add4ebc130
Niharika CNR <niharika-cnr@users.noreply.github.com>
2017-03-08 16:16:15 +0530

    Update multisort.c

--- multisort.c
+++ multisort.c
@@ -2,53 +2,56 @@
 #include <stdlib.h>
 #include <pthread.h>
 
-//the number of threads required--> 2 to sort, and 1 to merge
+/* the number of threads required are 3:- 2 to sort, and 1 to merge */
 #define THREADS 3
 
-void sort(void *params);
-void merge(void *params);
+/* function prototypes */
+void* sort(void *params);
+void* merge(void *params);
 
-//array to store the values input by the user
+/* array to store the values input by the user */
 int *array;
-//array to store the sorted values
+
+/* array to store the sorted values */
 int *sortedArray;
 
-//struct to store the START of the thread
+/* struct to store the START of the thread */
 typedef struct	{
 	int start;
 	int end;
 } parameters;
 
 
-int main()	{
+int main(int argc, char **argv)	{
 
-	//thread ID declaration
+	/* thread ID declaration */
 	pthread_t sortThread[THREADS];
 
-	//variable to store the SIZE of array, and a looping variable
-	int SIZE,i;
+	/* variable to store the SIZE of array, and a looping variable */
+	int SIZE=argc-1,i;
 
-	//taking the size of the array from user
-	printf("Enter the size of the array: ");
-	scanf("%d", &SIZE);	
-
-	//allocating necessary space to the int pointers to store array elements
-	array = (int *) malloc(SIZE*sizeof(int));
-	sortedArray = (int *) malloc(SIZE*sizeof(int));
-
-	//user inputs array values
-	for(i=0 ; i<SIZE ; i++)	{
-		printf("array[%d] = ",i);
-		scanf("%d",&array[i]);
+	if(argc==1)	{
+		printf("Usage: ./a.out <array elements separated by spaces>\n\n");
+		exit(0);
 	}
 	
-	/* The first half of array goes into the first thread to get sorted */
+	/* allocating necessary space to the int pointers to store array elements */
+	array = (int *) malloc(SIZE*sizeof(int));
+	sortedArray = (int *) malloc(SIZE*sizeof(int));
+	
+	//storing the 
+	for(i=0 ; i<SIZE ; i++)
+		array[i] = atoi(argv[i+1]);
+
+	
+	/* The first half of the array goes into the first thread to get sorted */
 	parameters *firstHalf = (parameters *) malloc(sizeof(parameters));
 	firstHalf->start = 0;
 	firstHalf->end = SIZE/2;
 
 	/* 1st thread is created with tid=sortThread[0]*/
 	pthread_create(&sortThread[0],NULL,sort,firstHalf);
+	
 	
 	/* The second half of array goes into the second thread to get sorted */
 	parameters *secondHalf = (parameters *) malloc(sizeof(parameters));
@@ -58,14 +61,16 @@ int main()	{
 	/* 2nd thread is created with tid=sortThread[1]*/
 	pthread_create(&sortThread[1],NULL,sort,secondHalf);
 	
+	
 	/* wait till both halves are sorted */
 	pthread_join(sortThread[0],NULL);
 	pthread_join(sortThread[1],NULL);
 	
+	
 	/* whole size of the array is given to the sortedArray thread */
 	parameters *fullArray = (parameters *) malloc(sizeof(parameters));
 	fullArray->start = 0;
-	fullArray->end = SIZE;
+	fullArray->end   = SIZE;
 
 	/* 3rd thread is created with tid=sortThread[2] */
 	pthread_create(&sortThread[2],NULL,merge,fullArray);	
@@ -77,51 +82,60 @@ int main()	{
 	printf("\nThe sorted array is: \n");
 	for(i=0 ; i<SIZE ; i++)
 		printf("sortedArray[%d] = %d\n",i,sortedArray[i]);
+	printf("\n\n");
 }//end of main()
 
-/* The sorting logic to be used by the threads to sort the halves */
-void sort(void *params)	{
+/* The sorting logic to be used by the threads to sort the halves: BUBBLE SORT */
+void* sort(void *params)	{
 
-	/* local variable to store the lower and upper bounds provided */
+	/* local variable to store the lower and upper bounds of the array to be sorted */
 	parameters *p = (parameters *)params;	
 	int begin = p->start;
-	int 	end = p->end;
+	int   end = p->end;
 	
-	int i,j,t;
+	int i,j,temp;
 	
 	for (i = begin; i < end; ++i)	{
 		for (j = i + 1; j < end; ++j)	{
          if (array[i] > array[j])	{
-         	t =  array[i];
+		     		 temp = array[i];
             array[i] = array[j];
-            array[j] = t;
+            array[j] = temp;
          }//if ends
       }//inner-for ends
-   }//outer-for ends  
+   }//outer-for ends
 }//end of sort()
 
 
 /* The merging logic to be used by the thread to merge the sorted halves */
-void merge(void *params)	{
+void* merge(void *params)	{
 
 	/* local variable to store the lower and upper bounds provided */
 	parameters *p = (parameters *)params;
-	int i = p->start;
-	int j =(p->end)/2;
+	
+	int 	  first = p->start;
+	int fhalf_end = p->end/2;
+	int 		last = p->end;
+	int shalf_end = last;
+	
+	int i = first;
+	int j = fhalf_end;
 	int k;
 			
 	/* Merges both the sorted halves */
-	for(k=0 ; k<(p->end)-1 ; k++)	{
-	
-		//left-half element less than right-half element
+	for(k=0 ; k<last ; k++)	{
+		
+		if(i>=fhalf_end || j>=shalf_end)
+			break;
+
+		//left-half element lesser than right-half element			
 		if(array[i]<array[j])
 			sortedArray[k]=array[i++];
 			
 		//left-half element greater than right-half element	
 		else if(array[i]>array[j])
 			sortedArray[k]=array[j++];
-			
-		//left-half element equal to right-half element
+		
 		else	{
 			sortedArray[k++]=array[j++];
 			sortedArray[k]=array[i++];
@@ -129,8 +143,9 @@ void merge(void *params)	{
 	}//for-loop ends
 
 	/* Prints the remaining last element */
-	if(i<(p->end/2))
-		sortedArray[k]=array[i];
-	else
-		sortedArray[k]=array[j];
+	while(i<fhalf_end)
+		sortedArray[k++]=array[i++];
+		
+	while(j<shalf_end)
+		sortedArray[k++]=array[j++];
 }//end of merge()
